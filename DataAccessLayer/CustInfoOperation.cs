42c83955fa04d602aa7e33070537315f7df2dc22
sam7022244383 <83719693+sam7022244383@users.noreply.github.com>
2022-07-31 22:49:30 +0530

    Add files via upload
    added dataAccess layer executable project

---
+++ DataAccessLayer/CustInfoOperation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccessLayer
+{
+    public class CustInfoOperation 
+    {
+        private string  Connection;
+
+        
+        public CustInfoOperation()
+        {
+            DbConnection con = new DbConnection();
+            Connection = con.GetConnection();
+        }
+
+       
+
+        public bool InsertCust(CustInfo Cust)
+        {
+            int executedStatement = 0;
+            using(SqlConnection sqlcon = new SqlConnection(Connection))
+            {
+                SqlCommand cmd = new SqlCommand("InsertCustomerData", sqlcon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MobileNumber",Cust.MobileNumber);
+                cmd.Parameters.AddWithValue("@FirstName",Cust.FirstName);
+                cmd.Parameters.AddWithValue("@LastName",Cust.LastName);
+                cmd.Parameters.AddWithValue("@PanNumber",Cust.PANNumber);
+                cmd.Parameters.AddWithValue("@AadharNumber",Cust.Aadhar_Card_Number);
+                cmd.Parameters.AddWithValue("@CustState",Cust.State);
+                cmd.Parameters.AddWithValue("@CustCity",Cust.CIty);
+                cmd.Parameters.AddWithValue("@AddressInAadhar",Cust.Address_InAadhar);
+                cmd.Parameters.AddWithValue("@Ocupation",Cust.Ocupation);
+                cmd.Parameters.AddWithValue("@ApplicationStatus","Pending");
+                sqlcon.Open();
+                executedStatement = cmd.ExecuteNonQuery();
+                if(executedStatement >0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+
+        }
+
+    }
+}
---
+++ DataAccessLayer/DbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataAccessLayer
+{
+    internal class DbConnection
+    {
+        public string  GetConnection()
+        {
+            //string connection;
+            //connection = ConfigurationManager.AppSettings["SavingAccountDB"].ToString();
+            //return connection;
+
+            string connection;
+            connection = "data source =LAPTOP-H2IJ3S8S\\SQLEXPRESS; Initial Catalog = SavingAccount; Integrated Security = True;";
+            return connection;
+            
+        }
+    }
+}
