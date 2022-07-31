aa7e23d6688b0f742aaf2fc01a8ef027f98028e1
sam7022244383 <83719693+sam7022244383@users.noreply.github.com>
2022-07-31 22:48:17 +0530

    Add files via upload
    added Business Logic Executable project

---
+++ BusinessLogic/CustInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class CustInfo
+    {
+        public string MobileNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PANNumber { get; set; }
+
+        public string Aadhar_Card_Number { get; set; }
+
+        public string State { get; set; }
+
+        public string CIty { get; set; }
+
+        public string Address_InAadhar { get; set; }
+
+        public string Ocupation { get; set; }
+
+       // public string Status { get; set; }
+
+    }
+}
