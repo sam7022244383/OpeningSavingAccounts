2c14ac8e904488ed155f4b30fe57c8d3e22f358a
sam7022244383 <samimujawar7@gmail.com>
2022-07-31 22:41:13 +0530

    Add project files.

---
+++ Controllers/CustController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+using BusinessLogic;
+
+namespace OpeningSavingAccounts.Controllers
+{
+    public class CustController : Controller
+    {
+        
+        // GET: Cust
+        [HttpGet]
+        public ActionResult CreateCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCustomer(CustInfo cust)
+        {
+            CustInfoOperation customeroperation = new CustInfoOperation();
+            bool result = customeroperation.InsertCust(cust);
+            if(result)
+            {
+                ModelState.Clear();
+                return View();
+            }
+            return View();
+            
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpeningSavingAccounts.MvcApplication" Language="C#" %>
